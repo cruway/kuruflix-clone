2a14b9829335c75ae10df40bb18891458fceea2d
sim wook <astralkaiser89@gmail.com>
2022-10-11 19:53:15 +0900

    animation practice

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 
 const Wrapper = styled(motion.div)`
-  height: 100vh;
   width: 100vw;
+  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -12,17 +12,60 @@ const Wrapper = styled(motion.div)`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 10px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
+
+const Circle = styled(motion.div)`
+  background-color: white;
+  width: 70px;
+  height: 70px;
+  place-self: center;
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+    start: {
+        opacity: 0,
+        scale: 0.5
+    },
+    end: {
+        opacity: 1,
+        scale: 1,
+        transition: {
+            type: "spring",
+            duration: 0.5,
+            bounce: 0.5,
+            delayChildren: 0.5,
+            staggerChildren: 0.2,
+        }
+    }
+}
+
+const circleVariants = {
+    start: {
+        opacity: 0,
+        y: 10
+    },
+    end: {
+        opacity: 1,
+        y: 0
+    },
+}
 
 function App() {
   return (
       <Wrapper>
-        <Box
-            transition={{ type: "spring", damping: 5 }}
-            initial={{ scale: 0 }} animate={{ scale: 1, rotateZ: 360 }} />
+        <Box variants={boxVariants} initial={"start"} animate={"end"} >
+            <Circle variants={circleVariants} />
+            <Circle variants={circleVariants} />
+            <Circle variants={circleVariants} />
+            <Circle variants={circleVariants} />
+        </Box>
       </Wrapper>
   );
 }
