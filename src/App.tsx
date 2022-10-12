9769e2901d0ddd0f240cc23a62c0185eb17e1b2b
sim wook <astralkaiser89@gmail.com>
2022-10-12 23:22:06 +0900

    animation practice

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const BiggerBox = styled.div`
 
 const Box = styled(motion.div)`
   width: 200px;
-  height: 200px;\
+  height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
