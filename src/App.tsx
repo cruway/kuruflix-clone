bb4ddb9cfb85046c8e2cf07ad4a9749514661447
sim wook <astralkaiser89@gmail.com>
2022-10-15 11:15:50 +0900

    kuruflix ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./Routes/Home";
+import Tv from "./Routes/Tv";
+import Search from "./Routes/Search";
+import Header from "./Routes/Components/Header";
 
 function App() {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
-                <Route path={"/"}></Route>
-                <Route path={"/tv"}></Route>
-                <Route path={"/search"}></Route>
+                <Route path={"/"}>
+                    <Home />
+                </Route>
+                <Route path={"/tv"}>
+                    <Tv />
+                </Route>
+                <Route path={"/search"}>
+                    <Search />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
